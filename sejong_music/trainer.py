81b33d279e8d48f28606b8060658faa6fa03f6ad
danbinaerinHan <danbinaerin@naver.com>
2024-04-08 22:48:17 +0900

    try except으로 바꿈

--- sejong_music/trainer.py
+++ sejong_music/trainer.py
@@ -590,7 +590,11 @@ class JeongganTrainer(Trainer):
       input_part_idx = sample[0][-1]
       target_part_idx = self.model.tokenizer.vocab[tgt[0][-1].item()]
       # if input_part_idx < 2: continue
-      src, output, (attn, output_tensor, _) = self.inferencer.inference(sample.to(self.device), target_part_idx)
+      try:
+        src, output, (attn, output_tensor, _) = self.inferencer.inference(sample.to(self.device), target_part_idx)
+      except:
+        print(f"Error occured in inference result: {e}")
+        # print([note[2] for note in output])
       try:
         jg_note_acc = per_jg_note_acc(output_tensor[:,0], shifted_tgt, inst=target_part_idx, tokenizer=self.inferencer.tokenizer)
         f1, prec, recall = onset_f1(output_tensor[:,0], shifted_tgt, inst=target_part_idx, tokenizer=self.inferencer.tokenizer)
